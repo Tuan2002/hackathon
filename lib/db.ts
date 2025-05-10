adf637362d49b2c3286568e7043fe6861bef2cde
Nguyen Ngoc Anh Tuan <tomtepfa@gmail.com>
2025-05-10 21:59:53 +0700

    refactor: enhance PrismaClient initialization with logging options

--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,12 @@
-'use server'
 import { PrismaClient } from "@prisma/client"
 
 declare global {
   var prisma: PrismaClient | undefined
 }
 
-export const db = globalThis.prisma || new PrismaClient()
+export const db = globalThis.prisma || new PrismaClient(
+  { log: ["query", "info", "warn", "error"] }
+);
 
 if (process.env.NODE_ENV !== "production") {
   globalThis.prisma = db
